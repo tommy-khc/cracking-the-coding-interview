be214ed9d93cd81bc6dfa467a6c6cc4cfb0e5dc0
tomycc <tom123abc@gmail.com>
2022-04-09 17:18:53 -0700

    CheckPermutation is completed

--- src/main/java/ch1/CheckPermutation.java
+++ src/main/java/ch1/CheckPermutation.java
@@ -3,37 +3,40 @@ package ch1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CheckPermutation {
 
     private static final Logger logger = LogManager.getLogger(CheckPermutation.class);
 
-//    public static Boolean answer (String str1, String str2) {
-//
-//        //convert char[] to List<character>
-//
-//        if (str1 == null || str2 == null) {
-//            return null;
-//        }
-//
-//        char[] cArr1 = str1.toCharArray();
-//        char[] cArr2 = str2.toCharArray();
-//
-//
-//
-//        for (char c1 : cArr1) {
-//            for (char c2 : cArr2) {
-//                if (c1 == c2) {
-//                    cArr1.
-//                }
-//            }
-//        }
-//
-//    }
+    //convert char[] to List<character>
+    public static Boolean methodTwo (String str1, String str2) {
+
+        if (str1 == null || str2 == null) {
+            return null;
+        }
+
+        char[] cArr1 = str1.toCharArray();
+        char[] cArr2 = str2.toCharArray();
+
+        List<Character> characters1 = new ArrayList<>();
+        List<Character> characters2 = new ArrayList<>();
+
+        for (char c1 : cArr1) {
+            characters1.add(c1);
+        }
+
+        for (char c2 : cArr2) {
+            characters2.add(c2);
+        }
+
+        return checkPre(characters1,characters2);
+    }
 
     //use for loop with index
-    public static Boolean answer (String str1, String str2) {
+    public static Boolean methodOne(String str1, String str2) {
 
         if (str1 == null || str2 == null) {
             return null;
@@ -60,5 +63,20 @@ public class CheckPermutation {
         }
 
         return Arrays.equals(cArr1, cArr2);
+    }
+
+    public static Boolean checkPre (List<Character> cList1, List<Character> cList2) {
+
+        for (Character c1 : cList1) {
+            for (Character c2 : cList2) {
+                if (c1 == c2) {
+                    cList1.remove(c1);
+                    cList2.remove(c2);
+                    return checkPre(cList1, cList2);
+                }
+            }
+        }
+
+        return cList1.equals(cList2);
     }
 }
--- src/test/java/ch1/CheckPermutationTest.java
+++ src/test/java/ch1/CheckPermutationTest.java
@@ -16,14 +16,30 @@ public class CheckPermutationTest {
     public void questionOne () {
         String str1 = "tommy";
         String str2 = "mytom";
-        Assertions.assertEquals(Boolean.TRUE, CheckPermutation.answer(str1, str2));
+        Assertions.assertEquals(Boolean.TRUE, CheckPermutation.methodOne(str1, str2));
     }
 
     @Test
     public void questionTwo () {
         String str1 = "tommy";
         String str2 = "tititndsdkkl";
-        Assertions.assertEquals(Boolean.FALSE, CheckPermutation.answer(str1, str2));
+        Assertions.assertEquals(Boolean.FALSE, CheckPermutation.methodOne(str1, str2));
+
+    }
+
+    //method two
+    @Test
+    public void questionOneForMethodTwo () {
+        String str1 = "tommy";
+        String str2 = "mytom";
+        Assertions.assertEquals(Boolean.TRUE, CheckPermutation.methodTwo(str1, str2));
+    }
+
+    @Test
+    public void questionTwoForMethodTwo () {
+        String str1 = "tommy";
+        String str2 = "tititndsdkkl";
+        Assertions.assertEquals(Boolean.FALSE, CheckPermutation.methodTwo(str1, str2));
 
     }
 }
