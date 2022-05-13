cc6c49a541b02532eb4d1bbcdeb8e94322dca000
tomycc <tom123abc@gmail.com>
2022-05-12 21:40:28 -0700

    ZeroMatrixTest, add remarks

--- src/test/java/ch1/ZeroMatrixTest.java
+++ src/test/java/ch1/ZeroMatrixTest.java
@@ -14,4 +14,26 @@ public class ZeroMatrixTest {
         int[][] expectedAnswer = {{1,0,3},{0,0,0},{7,0,9}};
         Assertions.assertEquals(Arrays.deepToString(expectedAnswer), Arrays.deepToString(answer));
     }
+
+    @Test
+    public void remarkOne() {
+
+        // point to diff address for other objects except array
+        int n = 3;
+        int k = n;
+        n = 7;
+        System.out.println(n);
+        System.out.println(k);
+    }
+
+    @Test
+    public void remarkTwo() {
+
+        //point to same address in array
+        int[][] n = {{1,2,3} , {4,5,6}};
+        int[][] k = n;
+        k[0] = new int[]{0, 0, 0};
+        System.out.println(Arrays.deepToString(n));
+        System.out.println(Arrays.deepToString(k));
+    }
 }
