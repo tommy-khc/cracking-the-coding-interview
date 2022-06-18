0ef44932ed9272461db4dc4a4d6939d9ef915313
tomycc <tom123abc@gmail.com>
2022-06-17 19:32:44 -0700

    LoopDetectable

--- src/main/java/answerFromBook/ch2/LoopDetectable.java
+++ src/main/java/answerFromBook/ch2/LoopDetectable.java
@@ -6,13 +6,40 @@ public interface LoopDetectable {
     //2. Move FastPointer at rate of 2 steps and SlowPointer at a rate of 1 step.
     //3. When they collide, move SlowPointer to LinkedListHead. Kepp FastPointer where it is.
     //4. Move SlowPointer and FastPointer at a rate of one step. Return the new collision point.
+//    default Node searchBeginningOfLoop (Node head) {
+//
+//        if (head == null) {
+//            return null;
+//        }
+//
+//        Boolean hasALoop = Boolean.FALSE;
+//        Node slow = head;
+//        Node fast = head;
+//
+//        while (fast != null) {
+//            slow = slow.next;
+//            fast = fast.next.next;
+//
+//            if (slow.equals(fast)) { //collide
+//                hasALoop = Boolean.TRUE;
+//                break;
+//            }
+//        }
+//
+//        if (hasALoop) {
+//            slow = head;
+//            while (!slow.equals(fast)) {
+//                slow = slow.next;
+//                fast = fast.next;
+//            }
+//            return slow;
+//        }
+//
+//        return null; //No loop
+//    }
+
     default Node searchBeginningOfLoop (Node head) {
 
-        if (head == null) {
-            return null;
-        }
-
-        Boolean hasALoop = Boolean.FALSE;
         Node slow = head;
         Node fast = head;
 
@@ -21,19 +48,20 @@ public interface LoopDetectable {
             fast = fast.next.next;
 
             if (slow.equals(fast)) { //collide
-                hasALoop = Boolean.TRUE;
+                break;
             }
         }
 
-        if (hasALoop) {
-            slow = head;
-            while (slow != fast) {
-                slow = slow.next;
-                fast = fast.next;
-            }
-            return slow;
+        if (fast == null || fast.next == null) { //No loop
+            return null;
         }
 
-        return null; //No loop
+        slow = head;
+        while (!slow.equals(fast)) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+
     }
 }
