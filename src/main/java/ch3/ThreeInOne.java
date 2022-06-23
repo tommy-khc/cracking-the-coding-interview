85519b0595711829a5f229dd00dae1317d5b0805
tomycc <tom123abc@gmail.com>
2022-06-22 18:50:32 -0700

    ThreeInOne and its test

--- src/main/java/ch3/ThreeInOne.java
+++ src/main/java/ch3/ThreeInOne.java
@@ -1,46 +1,91 @@
 package ch3;
 
+import java.util.Arrays;
+
 public class ThreeInOne {
 
-    private int[] ints = new int[3];
+    //single stack in an array
+    private int size = 3;
+
+    private Object[] objects = new Object[size];
 
     public ThreeInOne() {
     }
 
-    public int pop () {
-        for (int i = 0 ; i < 3; i++) {
-            if (ints[i] != 0) {
-                int result = ints[i];
-                ints[i] = 0;
+    public Object pop () {
+        for (int i = size-1 ; i >= 0; i--) {
+            if (objects[i] != null) {
+                Object result = objects[i]; //bug need to clone
+                objects[i] = 0;
                 return result;
             }
         }
-        return 0; //isEmpty
+        return null; //isEmpty
     }
 
-    public int peek () {
-        for (int i = 0 ; i < 3; i++) {
-            if (ints[i] != 0) {
-                return ints[i];
+    public Object peek () {
+        for (int i = size-1 ; i >= 0 ; i--) {
+            if (objects[i] != null) {
+                return objects[i];
             }
         }
-        return 0; //isEmpty
+        return null; //isEmpty
     }
 
     public void push (int n) {
-        for (int i = 2 ; i > 0; i--) {
-            if (ints[i] == 0) {
-                ints[i] = n;
+        if (isFull()) {
+            addArraysSize();
+        }
+        for (int i = 0 ; i < size; i++) {
+            if (objects[i] == null) {
+                objects[i] = n;
+                break;
             }
         }
     }
 
-    public boolean isEmpty () {
-        for (int i : ints) {
-            if (i != 0) {
+    public Boolean isEmpty () {
+        for (Object i : objects) {
+            if (i != null) {
                 return Boolean.FALSE;
             }
         }
         return Boolean.TRUE;
     }
+
+    public Boolean isFull () {
+        for (Object i : objects) {
+            if (i == null) {
+                return Boolean.FALSE;
+            }
+        }
+        return Boolean.TRUE;
+    }
+
+    //input: ints (array)
+    //output: void
+    //function: add size to ints (i.e. create an array which has larger size and replace the original one, same data)
+    // update int size
+    private void addArraysSize () {
+        Object[] replace = new Object[size*2];
+
+        for (int i = 0 ; i < size ; i++) {
+            replace[i] = objects[i];
+        }
+
+        size = replace.length;
+        objects = replace;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreeInOne{" +
+                "ints=" + Arrays.toString(objects) +
+                '}';
+    }
+
+    public int getSize() {
+        return size;
+    }
+
 }
--- src/test/java/ch3/ThreeInOneTest.java
+++ src/test/java/ch3/ThreeInOneTest.java
@@ -1,4 +1,21 @@
 package ch3;
 
+import org.junit.jupiter.api.Test;
+
 public class ThreeInOneTest {
+
+    //single stack in an array
+    @Test
+    public void testOne () {
+        ThreeInOne stack = new ThreeInOne();
+        int number = 0;
+        for (int i : new int[10]) {
+            stack.push(number);
+            number++;
+        }
+        System.out.println(stack.toString());
+        for (int i : new int[stack.getSize()]) {
+            System.out.println(stack.pop().toString());
+        }
+    }
 }
