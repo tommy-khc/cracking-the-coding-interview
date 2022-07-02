bc4399d8f13cbb0bfd48ec885e9011bf71a7b604
tomycc <tom123abc@gmail.com>
2022-07-02 12:52:19 -0700

    reload git account

--- src/test/java/ch3/ThreeInOneTest.java
+++ src/test/java/ch3/ThreeInOneTest.java
@@ -10,12 +10,12 @@ public class ThreeInOneTest {
         ThreeInOne stack = new ThreeInOne();
         int number = 0;
         for (int i : new int[10]) {
-            stack.push(number);
+            stack.push(number, "first"); // TODO Bug
             number++;
         }
         System.out.println(stack.toString());
         for (int i : new int[stack.getSize()]) { //bug, but that is not the point
-            System.out.println(stack.pop().toString());
+            System.out.println(stack.pop("first").toString());
         }
     }
 }
